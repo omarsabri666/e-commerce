6ca7d922f429809f47eadf649cfed23ab949167d
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2023-12-13 19:35:49 +0200

    Add files via upload

---
+++ assets/usePassword-60f6be5d.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-227f15af.js";function u(){const[t,e]=o.useState(!1);function r(){e(s=>!s)}const[a,g]=o.useState(!1);function n(){g(s=>!s)}return{togglePassword:t,togglePasswordVis:r,togglePassword2:a,togglePasswordVis2:n}}export{u};
